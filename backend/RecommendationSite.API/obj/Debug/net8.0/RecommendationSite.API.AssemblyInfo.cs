d37fcab10cf905d2a17ceca1ff352a48668b58a0
Andrew Malone <mandrew434@gmail.com>
2025-04-05 21:36:34 -0600

    First try updates

--- backend/RecommendationSite.API/obj/Debug/net8.0/RecommendationSite.API.AssemblyInfo.cs
+++ backend/RecommendationSite.API/obj/Debug/net8.0/RecommendationSite.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RecommendationSite.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d1e535abbbf9b4a3d50ab07a7e3a7d040dae8655")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+194dcb73d303d35059dbdd1b406606934e2b70b6")]
 [assembly: System.Reflection.AssemblyProductAttribute("RecommendationSite.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RecommendationSite.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
