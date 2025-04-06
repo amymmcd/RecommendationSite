5623a9854c48f91ff67bd5252e8b8667676ca314
Andrew Malone <mandrew434@gmail.com>
2025-04-05 21:38:18 -0600

    Split Content and Collaborative Filtering

--- backend/RecommendationSite.API/obj/Debug/net8.0/RecommendationSite.API.AssemblyInfo.cs
+++ backend/RecommendationSite.API/obj/Debug/net8.0/RecommendationSite.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RecommendationSite.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d928e9adee8b5d65cd0e9c92f32515a8d4739b7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+09e263a8db8a3a729d48f0f3ab47ffa086e7802c")]
 [assembly: System.Reflection.AssemblyProductAttribute("RecommendationSite.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RecommendationSite.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
